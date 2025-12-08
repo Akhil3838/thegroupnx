85d4740389c913f9476bce2b52e287b7a158c2fd
akhil3838 <akhiltv713@gmail.com>
2025-12-08 16:57:38 +0530

    page

--- app/page.js
+++ app/page.js
@@ -4,9 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
+      <h3>Pages</h3>
 <Link href={'/product'} >
         <h1> product list</h1>
   
-</Link>    </div>
+</Link>   
+<Link href={'/product-details'} >
+        <h1> product list</h1>
+  
+</Link>   
+
+ </div>
   );
 }
